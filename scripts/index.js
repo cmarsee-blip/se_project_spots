579af34322d87a89f7c198c29df366f44a5e7738
CodyMarsee <cmarsee@alumni.iu.edu>
2026-01-17 22:05:34 -0500

    Update Stage 8 break 2

--- scripts/index.js
+++ scripts/index.js
@@ -63,6 +63,12 @@ function getCardElement(data) {
     cardLikeBtnEl.classList.toggle("card__like-btn_active");
   });
 
+  const cardDeleteBtnEl = cardElement.querySelector(".card__delete-btn");
+  cardDeleteBtnEl.addEventListener("click", () => {
+    cardElement.remove();
+    cardElement = null;
+  });
+
   return cardElement;
 }
 
